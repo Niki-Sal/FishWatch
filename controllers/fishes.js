e92ab0a1e137b89b0f6e43954d87c554eaf7623a
niki-sal <ne.salehi@gmail.com>
2021-02-10 21:18:22 -0800

    setup favorite and more info views and routes

--- controllers/fishes.js
+++ controllers/fishes.js
@@ -14,6 +14,44 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/favorites',(req,res)=>{
+  db.fish.findAll().then((fishes)=>{
+    res.render('favorite', {fishes})
+  })
+})
+router.get('/:name', (req,res)=>{
+  let fishName = req.params.name.replace(' ','-')
+  console.log(fishName)
+  axios.get(`https://www.fishwatch.gov/api/species/${fishName}`)
+  
+  .then((response)=>{
+    console.log(response.data)
+    let resp = response.data
+    let respo = resp[0]
+    console.log(respo)
+    let name = respo['Species Name']
+    let harvest = respo['Harvest Type']
+    let image = respo['Species Illustration Photo']['src']
+    ////
+    const chosenFish = {
+      name,
+      harvest,
+      image
+    }
+    console.log(chosenFish)
+    res.render('moreInfo',{chosenFish})
+  })
+})
+
+router.post('/favorite/:name', (req, res)=>{
+  const userInput = req.params.name
+  console.log(userInput)
+  db.fish.create({
+    name: userInput
+  })
+  res.redirect('/fishes')
+})
+
 // router.get('/:param', (req, res)=> {
 //   db.user.findOne({ 
 //     where: {
